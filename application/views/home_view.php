2e0d4d5b9b32e35f3cebb3a3001277988e73ec66
Owen Corso <owen@ored.net>
2013-09-23 01:30:33 -0400

    EOD commit, i see a glimmer of light at the end of the tunnel... i'm hoping its not the headlight of the oncoming train.

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -141,7 +141,7 @@ lt-ie7"> <![endif]--> <!--[if IE 7]> <html class="no-js lt-ie9 lt- ie8">
 <a href="<?= $href; ?>" data-type="<?= $post->media_type; ?>" data-image="<?= $imagepath; ?>" data-id="<?= $post->id; ?>" data-title="<?= $post->title; ?>" data-client="<?= $post->client; ?>" data-detail-id="<?= $detail_name ?>" data-index="<?= $post->index; ?>" class="<?= $classes;?>">
 					<div id="rollover" class="<?= $overstatestyle; ?>">
 						<h1><?= $post->title; ?></h1>
-						<p><?= $post->description; ?></p>
+						<p><?= $post->tags == "soundcloud" ? $post->client : $post->description; ?></p>
 					</div>
 				</a>
 			<?php endforeach ?>
